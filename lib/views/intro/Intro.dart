c0103d18ad5cac261bd560e316e343e86ca29c8b
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2023-10-12 10:00:30 +0900

    파일분리

--- lib/views/intro/Intro.dart
+++ lib/views/intro/Intro.dart
@@ -1,3 +1,32 @@
 /* TODO : 시작페이지, 앱소개에 대한 정보 스크린
 Figma : 시작페이지
 */
+import 'package:flutter/material.dart';
+
+class Intro extends StatelessWidget {
+  const Intro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("School Market"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              "Hello, world",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
